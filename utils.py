0a36a30272176caf22ea0c4c3551b955334ae937
Meining89 <97919828+Meining89@users.noreply.github.com>
2023-09-11 21:33:41 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
-import numpy
 
 class utils:
     def reversed(num):
         
-        if(type(num) == int):
+        if(type(num) == int ):
 
             if(num < 0):
                 num = abs(num)
@@ -13,14 +12,23 @@ class utils:
 
             return reversed_num
 
-        else: print("Input is not a int number")
+        else: false = 'Input is not a int number' 
+        return false
+        
+    
 
 
     def formatter(num):
-        if(num.isnumeric() == True):
+        if(type(num) == int):
 
             binary = bin(num)
             octal = oct(num)
 
             return binary, octal
-        else: print("Input is not a int number")
+        else: false = 'Input is not a int number'
+        return false
+
+'''
+utils.reversed(1.2)
+'''
+#print(utils.reversed(17))
